70e6024d68b3112c549a883e6452af9711860cd1
Алексей Сергеевич Колташев <kas@inueco.ru>
2024-06-08 16:09:50 +0500

    Add new

--- design/index.php
+++ design/index.php
@@ -40,7 +40,7 @@
           <div class="col-7 mt-md-2">
             <p class="fs-1-suut fw-bold text-transform-uppercase line-height-small py-0 m-0">
               Графический <br />
-              и коммуникационный <br />
+              и информационный <br />
               дизайн
             </p>
           </div>
@@ -363,7 +363,119 @@
   </section>
 
   <!-- ЧТОБЫ УЧИТЬСЯ БЫЛО ПРИЯТНЕЕ -->
-  <?php include_once("../includes/section-for-funny-edu-common.inc.php") ?>
+  <section>
+        <div class="container mt-5">
+            <div class="mx-1 wrapper background-color-gray px-3 px-lg-4 px-xl-5 py-4 py-xl-5">
+                <div class="row">
+                    <p class="fs-1-suut fw-bold text-transform-uppercase">
+                        Чтобы учиться было приятнее
+                    </p>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/jivopis.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Мастерская живописи</h5>
+                                <p class="card-text text-dark-suut">
+                                    Здесь рождаются неповторимые шедевры
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/draw.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title" >Мастерская рисунка</h5>
+                                <p class="card-text text-dark-suut">
+                                  Всё, что нужно для создания произведения искусства
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                   
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/construirovanie.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Мастерская конструирования</h5>
+                                <p class="card-text text-dark-suut">
+                                  Для развития творческих способностей и пространственного мышления
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/stolov.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title" id="notComfortableStudentsCaffe">Уютное студенческое кафе</h5>
+                                <p class="card-text text-dark-suut">
+                                    Где вкусная и доступная еда
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/cafe.webp" class="card-img-top" id="notCoffeShop" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title" id="notCoffeShopText">Кофешоп</h5>
+                                <p class="card-text text-dark-suut">В каждом из корпусов</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/jalinga.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Студия записи Jalinga</h5>
+                                <p class="card-text text-dark-suut">
+                                    Для эффектных и интерактивных презентаций
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/sportzal.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Тренажерный зал</h5>
+                                <p class="card-text text-dark-suut">Для всех студентов</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/comp_grafica.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Мастерская компьютерной графики</h5>
+                                <p class="card-text text-dark-suut">С современным аппаратным и программным обеспечением
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 mb-4 px-3 px-lg-5">
+                        <div class="card card-suut h-100" style="width: 100%">
+                            <img src="./img/coworking.webp" class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Зона коворкинга</h5>
+                                <p class="card-text text-dark-suut">Для командной работы, общения или отдыха</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
   <!-- ПРАКТИКА СТУДЕНТОВ -->
   <section>
